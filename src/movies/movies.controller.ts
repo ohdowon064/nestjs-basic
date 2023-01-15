a3084e40d1e11f22a79045caed53e084aa724a35
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-15 16:38:41 +0900

    body, query data

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,23 +16,29 @@ export class MoviesController {
     return 'all movies';
   }
 
-  @Get('/:id')
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `search movie with title after: ${searchingYear}`;
+  }
+
+  @Get(':id')
   getOne(@Param('id') movieId: string) {
     return `one movie: ${movieId}`;
   }
 
   @Post()
-  createOne() {
-    return 'create movie';
+  createOne(@Body() movieData) {
+    console.log(movieData);
+    return movieData;
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   deleteOne(@Param('id') movieId: string) {
     return `delete movie: ${movieId}`;
   }
 
-  @Patch('/:id')
-  updateOne(@Param('id') movieId: string) {
-    return `update movie: ${movieId}`;
+  @Patch(':id')
+  updateOne(@Param('id') movieId: string, @Body() updateData) {
+    return { updateMovie: movieId, ...updateData };
   }
 }
